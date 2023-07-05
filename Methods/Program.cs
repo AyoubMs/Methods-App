91358b0a33439afd056e1e2eb0b598ea5251c394
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:17:13 +0200

    add app

---
+++ Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Methods;
+
+//SampleMethods.SayHi();
+//WriteLine();
+//for (int i = 0; i < 10; i++)
+//{
+//    SampleMethods.SayHi();
+//}
+
+// DRY - Don't Repeat Yourself
+// SOLID - SRP - Single Responsibility Principle
+
+//string name = ConsoleMessages.GetUsersName();
+
+//ConsoleMessages.SayHi(name);
+
+//double results = MathShortcuts.Add(5, 3);
+//Console.WriteLine($"The result is {results}");
+
+//double[] vals = new double[] { 1, 2, 3, 4, 5 };
+
+//MathShortcuts.AddAll(vals);
+
+//ConsoleMessages.SayGoodBye();
+
+// Discard character (_)
+(string firstName, _) = ConsoleMessages.GetFullName();
+
+Console.WriteLine($"First name: {firstName}");
+//Console.WriteLine($"Last name: {lastName}");
+
